6836495a217b15e3c9ef2593c22b84c21211f236
Herbi Tarik <herbitarik2509@gmail.com>
2023-04-19 20:22:06 +0200

    Creating the home screen

--- app/index.js
+++ app/index.js
@@ -1,34 +1,96 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native'
+import {
+    FontAwesome,
+    Entypo,
+    MaterialCommunityIcons,
+    FontAwesome5,
+    Ionicons,
+    MaterialIcons,
+} from '@expo/vector-icons'
+import car from '../assets/images/car.png'
+import menuOptions from '../assets/menuOptions'
 
 export default function Page() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
-      </View>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <View>
+                    <Text style={styles.title}>My model S</Text>
+                    <Text style={styles.subtitle}>Parked</Text>
+                </View>
+                <FontAwesome name="user-circle" size={30} color="gray" />
+            </View>
+            <Image source={car} style={styles.image} resizeMode="contain" />
+            <View style={styles.controls}>
+                <Entypo name="lock" size={26} color="gray" />
+                <MaterialCommunityIcons name="fan" size={26} color="gray" />
+                <FontAwesome5 name="bolt" size={26} color="gray" />
+                <Ionicons name="car-sport-sharp" size={26} color="gray" />
+            </View>
+
+            <FlatList
+                data={menuOptions}
+                renderItem={({ item }) => (
+                    <View style={styles.optionRow}>
+                        <MaterialCommunityIcons
+                            name={item.iconName}
+                            size={26}
+                            color="gray"
+                        />
+                        <Text style={styles.optionText}>{item.name}</Text>
+                        <MaterialIcons
+                            name="keyboard-arrow-right"
+                            size={24}
+                            color="gray"
+                            style={{ marginLeft: 'auto' }}
+                        />
+                    </View>
+                )}
+            />
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-});
+    container: {
+        flex: 1,
+        padding: 24,
+        backgroundColor: '#161818',
+    },
+    header: {
+        marginTop: 50,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    title: {
+        fontSize: 24,
+        color: '#eee',
+        fontWeight: 'bold',
+        marginBottom: 8,
+    },
+    subtitle: {
+        color: 'gray',
+        fontWeight: '500',
+    },
+    image: {
+        width: '100%',
+        height: 300,
+    },
+    controls: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        marginVertical: 20,
+    },
+    optionRow: {
+        flexDirection: 'row',
+        marginVertical: 20,
+        alignItems: 'center',
+    },
+    optionText: {
+        color: '#eee',
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginLeft: 10,
+    },
+})
